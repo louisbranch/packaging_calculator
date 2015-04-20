da353822e55c3a87c26ba832d354a65f114cfbd6
Luiz Branco <me@luizbranco.com>
2015-04-20 08:18:38 -0400

    Add markup by product

--- test/calc_test.js
+++ test/calc_test.js
@@ -6,7 +6,7 @@ describe("NuPack Calculator", function () {
   describe(".initialCost()", function () {
 
     it("adds a flat markup of 5%", function () {
-      assert.equal(calc.initialCost(1000.00), 1050);
+      assert.equal(calc.initialCost(1000), 1050);
     });
 
   });
@@ -14,7 +14,27 @@ describe("NuPack Calculator", function () {
   describe(".peopleCost()", function () {
 
     it("adds 1.2% for each person", function () {
-      assert.equal(1075.20, calc.peopleCost(1050, 2));
+      assert.equal(1024, calc.peopleCost(1000, 2));
+    });
+
+  });
+
+  describe(".productCost()", function () {
+
+    it("adds 7.5% for drugs", function () {
+      assert.equal(1075, calc.productCost(1000, "drugs"));
+    });
+
+    it("adds 13% for food", function () {
+      assert.equal(1130, calc.productCost(1000, "food"));
+    });
+
+    it("adds 2% for electronics", function () {
+      assert.equal(1020, calc.productCost(1000, "electronics"));
+    });
+
+    it("remains the same for everything else", function () {
+      assert.equal(1000, calc.productCost(1000, "books"));
     });
 
   });
